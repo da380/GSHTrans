027c797c4d879d7e36f290facf49b1818fcc494d
David <da380@cam.ac.uk>
2023-07-14 18:10:08 +0100

    work on legendre functions

--- examples/LegendreExample.cpp
+++ examples/LegendreExample.cpp
@@ -2,6 +2,7 @@
 #include <concepts>
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <numbers>
 
 
@@ -13,49 +14,29 @@ int main() {
 
   using std::cout;
   using std::endl;
-  cout.setf(std::ios_base::scientific);
-  cout.setf(std::ios_base::showpos);
-  cout.precision(16);
+  using namespace GSHT;
 
-  int n = 100;
-  int l = 200;
-  std::vector<Float> thetas(n);  
-
-  Float th{0.0};
-  Float dth = std::numbers::pi_v<Float>/static_cast<Float>(n-1);
-  for (auto& val : thetas)
-    {
-      val = th;
-      th += dth;
-    }
+  Float theta = 10;
+  int L = 4;
+  int M = 1;
   
+  LegendreValues plm(theta,L,M);
 
-  for(auto theta : thetas)
+
+  for(int l = 0; l <= L; l++)
     {
+      LegendreValues<Float>::iterator start = plm.begin(l);
+      LegendreValues<Float>::iterator finish = plm.end(l);
+      if(l < L)
+	{	  
+	  cout << *start <<  " " << *finish << endl;
+	}
+      else
+	{
+	  cout << *start << endl;
+	}
+    }
 
-      
-    // Construct the iterator.
-    GSHT::LegendreIterator p(theta);
 
-    // Take l steps.
-    p + l;
 
-    // Get a reference the current values
-    GSHT::LegendreIterator<Float>::reference plm = *p;
-
-    // Build vector of values using STL function.
-    std::vector<Float> plm2(l + 1);
-    std::generate(plm2.begin(), plm2.end(), [l, m = 0, theta]() mutable {
-      return std::sph_legendre(l, m++, theta);
-    });
-
-    // Compute the difference.
-    std::transform(plm.begin(), plm.end(), plm2.begin(), plm.begin(),
-                   std::minus<>());
-
-    // Write out the maximum value
-    auto max = std::abs(*std::max_element(plm.begin(), plm.end()));
-
-    cout << theta  << " " << max << endl;
-  }
 }
--- include/src/Legendre.h
+++ include/src/Legendre.h
@@ -4,12 +4,110 @@
 #include <cassert>
 #include <cmath>
 #include <concepts>
+#include <iterator>
 #include <limits>
 #include <numbers>
 #include <utility>
 #include <vector>
 
 namespace GSHT {
+
+template <std::floating_point Float>
+class LegendreValues {
+ public:
+  // Define member types.
+  using value_type = Float;
+  using iterator = typename std::vector<Float>::iterator;
+
+  // Constructors.
+  LegendreValues() = delete;
+  LegendreValues(Float theta, int L, int M);
+
+
+  // Basic data functions.
+  int MaxDegree() { return L; }
+  int MaxOrder() { return M; }
+  Float Angle() { return theta; }
+
+  // Functions to return iterators to the values.
+  iterator begin() { return data.begin(); }
+  iterator end() { return data.end(); }
+  iterator begin(int l) { return std::next(begin(), CountBeforeDegree(l)); }
+  iterator end(int l) { return std::next(begin(),CountToDegree(l)); }
+
+  // Function to return a specific value.
+  Float operator() (int l, int m)
+  {
+    auto p = *std::next(begin(l), m < 0 ? -m  : m);
+    return m < 0 ? Sign(m) * p : p;
+  }
+
+  
+ private:
+  int L;
+  int M;
+  Float theta;
+  std::vector<Float> data;
+  std::vector<std::pair<int,int>> index;
+
+  int CountToDegree(int l) {
+    return l <= M ? l + 1 + (l * (l + 1)) / 2
+                  : M + 1 + (M * (M + 1)) / 2 + (l - M) * (M + 1);
+  }
+
+  int CountBeforeDegree(int l) { return CountToDegree(l - 1); }
+
+  Float Sign(int m) { return m % 2 ? -1.0 : 1.0; }
+  
+};
+
+template <std::floating_point Float>
+LegendreValues<Float>::LegendreValues(Float theta, int L, int M)
+    : theta{theta}, L{L}, M{M} {
+  // Check the degree is non-negative.
+  assert(L >= 0);
+
+  // Check the maximum order is in range.
+  assert(M >= 0 && M <= L);
+
+  // Allocate space to store the values.
+  data.reserve(CountToDegree(L));
+
+
+  int count = 0;
+  for (int l = 0; l <= L; l++) {
+    for (int m = 0; m <= std::min(l, M); m++) {
+      data.push_back(count++);
+    }
+  }
+
+  /*
+
+  // Deal with degree 0.
+  {
+    constexpr Float f = 0.5 * std::numbers::inv_sqrtpi_v<Float>;
+    data.push_back(f);
+  }
+
+  // pre-compute trigonometric terms.
+  Float sin = std::sin(theta);
+  Float cos = std::cos(theta);
+
+  // Deal with degree 1.
+  if (L > 0) {
+    constexpr Float f =
+        0.5 * std::numbers::sqrt3_v<Float> * std::numbers::inv_sqrtpi_v<Float>;
+    constexpr Float g = -f / std::numbers::sqrt2_v<Float>;
+    data.push_back(f * cos);
+    data.push_back(g * sin);
+  }
+
+  // Deal with the remaining terms using recursion.
+  if (L > 1) {
+  }
+
+  */
+}
 
 template <std::floating_point Float>
 class LegendreIterator {
@@ -22,13 +120,9 @@ class LegendreIterator {
 
   // Define the constructors.
   LegendreIterator() = delete;
-  LegendreIterator(Float theta, int mMax = -1)
-      : l{-1}, theta{theta}, mMax{mMax} {
+  LegendreIterator(Float theta) : l{-1}, theta{theta} {
     Float min = std::numeric_limits<Float>::min();
     Float eps = std::numeric_limits<Float>::epsilon();
-    bool too_low = theta < -min;
-    bool too_high = theta > std::numbers::pi_v<Float> * (1 + eps);
-    assert(!too_low && !too_high);
   }
 
   // Return the degree.
@@ -65,11 +159,12 @@ class LegendreIterator {
     auto Fl = static_cast<Float>(l);
 
     // Increment orders m < l using recursion.
+    Float f1Pre = std::sqrt(4 * Fl * Fl - 1);
+    Float f2Pre = l > 1 ? 1.0 / std::sqrt(4 * (Fl - 1) * (Fl - 1) - 1) : 0.0;
     for (int m = 0; m < l; m++) {
       auto Fm = static_cast<Float>(m);
-      Float f1 = std::sqrt((4 * Fl * Fl - 1) / (Fl * Fl - Fm * Fm));
-      Float f2 = std::sqrt(((Fl - 1) * (Fl - 1) - Fm * Fm) /
-                           (4 * (Fl - 1) * (Fl - 1) - 1));
+      Float f1 = f1Pre / std::sqrt(Fl * Fl - Fm * Fm);
+      Float f2 = std::sqrt(((Fl - 1) * (Fl - 1) - Fm * Fm)) * f2Pre;
       previous[m] = f1 * (cos * current[m] - f2 * previous[m]);
       std::swap(previous[m], current[m]);
     }
@@ -109,8 +204,8 @@ class LegendreIterator {
   // Store the current degree.
   int l;
 
-  // Store the maximum order to be computed
-  int mMax;
+  // Store the maximum order
+  int M;
 
   // Store the angle and trig function values
   Float theta;
