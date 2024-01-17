1d559872ad7356d3e8a2911530e9159f474b8173
David <da380@cam.ac.uk>
2024-01-17 17:59:47 +0000

    Eigen3 out, Boost in

--- GSHTrans/src/Fields.h
+++ GSHTrans/src/Fields.h
@@ -9,18 +9,6 @@
 
 namespace GSHTrans {
 
-class UnaryPlus {
- public:
-  auto operator()(auto&& x) { return x; }
-  auto operator()(auto&& x) const { return x; }
-};
-
-class UnaryMinus {
- public:
-  auto operator()(auto&& x) { return -x; }
-  auto operator()(auto&& x) const { return -x; }
-};
-
 template <typename Derived>
 class ScalarFieldBase {
   using Int = std::ptrdiff_t;
@@ -46,100 +34,98 @@ class ScalarFieldBase {
     return GridReference().NumberOfLongitudes();
   }
 
-  // Return iterators.
-  auto begin() { return GetDerived()._Range().begin(); }
-  auto end() { return GetDerived()._Range().end(); }
+  // Return range.
+  auto DataView() { return GetDerived()._DataView(); }
 
-  auto cbegin() const { return GetDerived()._cbegin(); }
-  auto cend() const { return GetDerived()._cend(); }
+  // Return iterators.
+  auto begin() { return DataView().begin(); }
+  auto end() { return DataView().end(); }
 
   // Return size.
-  auto size() const { return GetDerived()._Range().size(); }
+  auto size() { return DataView().size(); }
 
   // Index operators
-  auto operator[](Int i) { return GetDerived()._Range()[i]; }
-  auto operator[](Int i) const { return GetDerived()._Index(i); }
+  auto operator[](Int i) { return DataView()[i]; }
 
   // Return value of the field at the (iTheta,iPhi)th location.
-  auto operator()(Int iTheta, Int iPhi) const {
+  auto operator()(Int iTheta, Int iPhi) {
     auto i = NumberOfLongitudes() * iTheta + iPhi;
     return operator[](i);
   }
 
   // Return the integral of the field.
-  auto Integrate() const {
-    auto start = GetDerived()._cbegin();
-    auto finish = GetDerived()._cend();
-    auto range = std::ranges::subrange(start, finish);
-    return GridReference().Integrate(range);
-  }
+  auto Integrate() { return GridReference().Integrate(DataView()); }
 
  private:
   // Return references to the instance of the derived class.
   auto& GetDerived() { return static_cast<Derived&>(*this); }
   const auto& GetDerived() const { return static_cast<const Derived&>(*this); }
-};
+  };
 
-template <typename Grid>
-class ScalarField : public ScalarFieldBase<ScalarField<Grid>> {
- public:
-  using real_type = Grid::real_type;
-  using scalar_type = Grid::scalar_type;
+  template <typename Grid>
+  class ScalarField : public ScalarFieldBase<ScalarField<Grid>> {
+   public:
+    using real_type = Grid::real_type;
+    using scalar_type = Grid::scalar_type;
 
- private:
-  using Int = std::ptrdiff_t;
-  using Vector = FFTWpp::vector<scalar_type>;
+   private:
+    using Int = std::ptrdiff_t;
+    using Vector = FFTWpp::vector<scalar_type>;
 
- public:
-  // Set default constructors.
-  ScalarField() = default;
-  ScalarField(const ScalarField&) = default;
-  ScalarField(ScalarField&&) = default;
+   public:
+    // Set default constructors.
+    ScalarField() = default;
+    ScalarField(const ScalarField&) = default;
+    ScalarField(ScalarField&&) = default;
 
-  // Constructor given a grid initialising values to zero.
-  ScalarField(std::shared_ptr<Grid> grid)
-      : _grid{grid},
-        _data{Vector(_grid->NumberOfCoLatitudes() * _grid->NumberOfLongitudes(),
-                     1)} {}
+    // Constructor given a grid initialising values to zero.
+    ScalarField(std::shared_ptr<Grid> grid)
+        : _grid{grid},
+          _data{Vector(
+              _grid->NumberOfCoLatitudes() * _grid->NumberOfLongitudes(), 1)} {}
 
-  // Constructor given grid parameters.
-  ScalarField(Int lMax, FFTWpp::PlanFlag flag = FFTWpp::Measure)
-      : _grid{std::make_shared<Grid>(lMax, 0, flag)},
-        _data{Vector(_grid->NumberOfCoLatitudes() * _grid->NumberOfLongitudes(),
-                     1)} {}
+    // Constructor given grid parameters.
+    ScalarField(Int lMax, FFTWpp::PlanFlag flag = FFTWpp::Measure)
+        : _grid{std::make_shared<Grid>(lMax, 0, flag)},
+          _data{Vector(
+              _grid->NumberOfCoLatitudes() * _grid->NumberOfLongitudes(), 1)} {}
 
-  // Copy constructor from other derived class.
-  template <typename OtherDerived>
-  ScalarField(const ScalarFieldBase<OtherDerived>& other)
-      : _grid{other.GridPointer()}, _data(other.cbegin(), other.cend()) {}
+    // Copy constructor from other derived class.
+    template <typename OtherDerived>
+    ScalarField(ScalarFieldBase<OtherDerived>& other)
+        : _grid{other.GridPointer()}, _data(other.begin(), other.end()) {}
 
-  // Set default assigment operators.
-  ScalarField& operator=(const ScalarField&) = default;
-  ScalarField& operator=(ScalarField&&) = default;
+    // Set default assigment operators.
+    ScalarField& operator=(ScalarField&) = default;
+    ScalarField& operator=(ScalarField&&) = default;
 
-  // Copy assignment from other derived class.
-  template <typename OtherDerived>
-  ScalarField& operator=(const ScalarFieldBase<OtherDerived>& other) {
-    this->_grid = other.GridPointer();
-    this->_data = Vector(other.cbegin(), other.cend());
-    return *this;
-  }
+    // Copy assignment from other derived class.
+    template <typename OtherDerived>
+    ScalarField& operator=(ScalarFieldBase<OtherDerived>& other) {
+      this->_grid = other.GridPointer();
+      this->_data = Vector(other.begin(), other.end());
+      return *this;
+    }
 
-  // Define the required public methods.
-  auto& _GridReference() const { return *_grid; }
-  auto _GridPointer() const { return _grid; }
+    // Define the required public methods.
+    auto& _GridReference() const { return *_grid; }
+    auto _GridPointer() const { return _grid; }
+    auto _DataView() { return std::ranges::views::all(_data); }
 
-  auto _Range() { return std::ranges::views::all(_data); }
+    // Define additional public methods that allow values to be set.
+    auto& operator[](Int i) { return _data[i]; }
 
-  auto _cbegin() const { return _data.cbegin(); }
-  auto _cend() const { return _data.cend(); }
+    auto& operator()(Int iTheta, Int iPhi) {
+      auto i = _grid->NumberOfLongitudes() * iTheta + iPhi;
+      return operator[](i);
+    }
 
-  // Define additional public methods.
+    // Define additional public methods.
 
- private:
-  std::shared_ptr<Grid> _grid;
-  Vector _data;
-};
+   private:
+    std::shared_ptr<Grid> _grid;
+    Vector _data;
+  };
 
 }  // namespace GSHTrans
 
--- examples/ScalarFieldExample.cpp
+++ examples/ScalarFieldExample.cpp
@@ -1,5 +1,11 @@
 #include <GSHTrans/All>
 #include <algorithm>
+#include <boost/assign.hpp>
+#include <boost/range/adaptor/reversed.hpp>
+#include <boost/range/adaptor/transformed.hpp>
+#include <boost/range/algorithm/copy.hpp>
+#include <boost/range/combine.hpp>
+#include <boost/tuple/tuple.hpp>
 #include <chrono>
 #include <cmath>
 #include <concepts>
@@ -12,16 +18,31 @@
 #include <random>
 
 int main() {
+  /*
+
   using namespace GSHTrans;
   using Real = double;
   using Complex = std::complex<Real>;
   using Grid = GaussLegendreGrid<Real, R2C>;
   using Scalar = Grid::scalar_type;
 
-  auto lMax = 8;
+  auto lMax = 4;
   auto nMax = 2;
 
-  auto grid = std::make_shared<Grid>(lMax, nMax);
+  auto f = ScalarField<Grid>(lMax);
 
-  auto f = ScalarField(grid);
+  for (auto& value : f) value = 2;
+
+  for (auto value : f) std::cout << value << std::endl;
+
+  */
+
+  auto x = std::vector<double>(10, 1);
+  auto y = std::vector<double>(10, 2);
+
+  auto z = boost::combine(x, y) | boost::adaptors::transformed([](auto pair) {
+             return boost::get<0>(pair) + boost::get<1>(pair);
+           });
+
+  for (auto value : z) std::cout << value << std::endl;
 }
