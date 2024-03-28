24c5d56105b2a3f6c3c98ff2a147e01ceaa933ac
David <da380@cam.ac.uk>
2024-03-28 18:36:05 +0000

    work on tensors

--- examples/ScalarFieldExample.cpp
+++ examples/ScalarFieldExample.cpp
@@ -1,27 +1,15 @@
 #include <GSHTrans/All>
+#include <array>
 #include <iostream>
+#include <ranges>
+#include <tuple>
 
 using namespace GSHTrans;
-
 int main() {
-  using Real = double;
-  using Complex = std::complex<Real>;
-  using Scalar = Real;
-  using MRange = All;
-  using NRange = All;
+  using namespace GSHTrans;
 
-  using Grid = GaussLegendreGrid<Real, MRange, NRange>;
-
-  auto lMax = 8;
-  auto nMax = 2;
-
-  auto grid = Grid(lMax, nMax);
-
-  auto [u, dataU] = AllocateRealField(grid);
-  auto [v, dataV] = AllocateComplexField(grid);
-
-  u = 1;
-  v = [](auto theta, auto phi) { return 2; };
-
-  for (auto val : real(u + v)) std::cout << val << std::endl;
+  for (auto index : TensorIndices<10>()) {
+    for (auto alpha : index) std::cout << alpha << " ";
+    std::cout << std::endl;
+  }
 }
